07fa82b6168cd1b766327f536bcc9c84be3563d7
Patrick Stein <lylepstein@gmail.com>
2013-08-30 04:37:59 -0700

    Non-homepage design work

--- site/public/static/js/main.js
+++ site/public/static/js/main.js
@@ -1,4 +1,38 @@
-window.addEventListener('scroll', function(e) {
+(function() {
+  var shroud = document.getElementById('shroud');
+  var shroudable = {
+    '/projects': '#FFB538',
+    '/blog/': '#51A9FF'
+  };
 
-}, false);
+  var transform = function(el, v) {
+    var s = el.style;
+    s.WebkitTransform =
+    s.MozTransform =
+    s.msTransform =
+    s.OTransform =
+    s.transform = v;
+  };
 
+  vz.touch(document.querySelectorAll('a'), {click: function(e) {
+    var href = e.target.getAttribute('href');
+
+    if (shroud && shroudable.hasOwnProperty(href)) {
+      shroud.style.transition = 'none';
+      shroud.offsetLeft;
+      shroud.style.opacity = 1;
+      transform(shroud, 'translateX(100%)');
+      shroud.style.backgroundColor = shroudable[href];
+      shroud.style.display = 'block';
+      shroud.style.transition = 'all 0.3s ease';
+      shroud.offsetLeft;
+      transform(shroud, '');
+
+      setTimeout(function() {
+        window.location = href;
+      }, 200);
+    } else {
+      window.location = href;
+    }
+  }});
+})();
